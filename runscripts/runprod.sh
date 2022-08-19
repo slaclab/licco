2b46728544c1c73098053b0c6bb1567dd06e02a6
Murali Shankar <mshankar@slac.stanford.edu>
2022-08-19 11:56:25 -0700

    Server IP port

--- runscripts/runprod.sh
+++ runscripts/runprod.sh
@@ -16,13 +16,10 @@ else
 fi
 
 
-# Of course, please change this port to the appropriate port in the 8000-1000 range.
-# Also change start:app to your_service:app (this should make it easier to identify your service amongst the pile of gunicorns)
-# Add a proxy in the wen servce to proxy this port onto the location for this service.
 export ACCESS_LOG_FORMAT='%(h)s %(l)s %({REMOTE_USER}i)s %(t)s "%(r)s" "%(q)s" %(s)s %(b)s %(D)s'
 export LOG_LEVEL=${LOG_LEVEL:-"INFO"}
+export SERVER_IP_PORT=${SERVER_IP_PORT:-"0.0.0.0:5000"}
 
-export SERVER_IP_PORT="0.0.0.0:5000"
 export PYTHONPATH="${PWD}/modules/flask_authnz":"${PYTHONPATH}"
 
 # The exec assumes you are calling this from supervisord. If you call this from the command line; your bash shell is proabably gone and you need to log in.
