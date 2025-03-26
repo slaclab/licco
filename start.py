ae7c53827a8fcde26f531a6f8e6039d2e7a1a6ab
GreatDanton <pribosek.jan@gmail.com>
2025-03-26 12:31:27 +0100

    Moved application configuration variables into a separate file. Refactored context.py

--- start.py
+++ start.py
@@ -1,30 +1,42 @@
-import configparser
-from json import JSONEncoder
-
-from flask import Flask, Response
+import argparse
+import app_config
 import logging
-import os
 import sys
-import json
+from json import JSONEncoder
+from flask import Flask, Response
 from flask_cors import CORS
 
 import context
-from notifications.email_sender import EmailSettings, EmailSender, EmailSenderMock
-from notifications.notifier import Notifier
-from pages import pages_blueprint
-from services.licco import licco_ws_blueprint
-from dal.mcd_model import initialize_collections
 
+# parse cli args
+parser = argparse.ArgumentParser(description="Licco CLI Options")
+parser.add_argument("-c", "--config", default="", help="Path to a Licco config file")
+args = parser.parse_args()
+
+# load configuration
+conf = app_config.load_config(args.config)
+print("=== APP CONFIG ===")
+print(conf)
+print("")
+
+# setup a global logger
 logger = logging.getLogger(__name__)
+root = logging.getLogger()
+root.setLevel(logging.getLevelName(conf.app_log_level))
+# filter out server heartbeat logs
+logging.getLogger('pymongo.topology').setLevel(level=logging.WARNING)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+root.addHandler(ch)
 
-# Initialize application.
+# init flask service
 app = Flask("licco")
 CORS(app, supports_credentials=True)
-# Set the expiration for static files
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 300
-
-app.secret_key = "A secret key for licco"
-app.debug = json.loads(os.environ.get("DEBUG", "false").lower())
+# set the expiration for static files
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = conf.app_send_file_max_age_default
+app.secret_key = conf.app_secret_key
+app.debug = str(conf.app_debug).lower()
 app.config["TEMPLATES_AUTO_RELOAD"] = app.debug
 
 # unexpected app exceptions are rendered as json
@@ -35,70 +47,14 @@ def handle_exception(e):
     return Response(out, status=500, mimetype="application/json")
 
 
-send_smtp_emails = os.environ.get("LICCO_SEND_EMAILS", False)
+# setup global variables (db, email notifiers, security helpers). Also migrates db
+context.init_context(conf)
 
-# read credentials file for notifications module
-credentials_file = os.environ.get("LICCO_CREDENTIALS_FILE", "./credentials.ini")
-
-if not os.path.exists(credentials_file):
-    print(f"'credentials.ini' file was not found (path: {credentials_file}), email user notifications are disabled")
-    app.config["NOTIFICATIONS"] = {"service_url": "http://localhost:3000"}
-    app.config["NOTIFICATIONS"]["email"] = {
-        "development_mode": not send_smtp_emails,
-        "admin_email": ""
-    }
-else:
-    config = configparser.ConfigParser()
-    config.read(credentials_file)
-    app.config["NOTIFICATIONS"] = {"service_url": config["email"]["service_url"]}
-
-    app.config["NOTIFICATIONS"]["email"] = {
-        "url": config["email"]["url"],
-        "port": config["email"]["port"],
-        "email_auth": config["email"].getboolean("email_auth"),
-        "user": config["email"]["user"],
-        "password": config["email"]["password"],
-        "admin_email": config["email"]["admin_email"],
-        "username_to_email_service": config["email"]["username_to_email_service"],
-        "development_mode": send_smtp_emails,
-    }
-
-root = logging.getLogger()
-root.setLevel(logging.getLevelName(os.environ.get("LOG_LEVEL", "INFO")))
-# filter out server heartbeat logs
-logging.getLogger('pymongo.topology').setLevel(level=logging.WARNING)
-ch = logging.StreamHandler(sys.stdout)
-ch.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-ch.setFormatter(formatter)
-root.addHandler(ch)
-
-# Register routes.
+# register routes (this should happen after initializing context, since controllers refer to those global variables)
+from pages import pages_blueprint              # noqa: E402
+from services.licco import licco_ws_blueprint  # noqa: E402
 app.register_blueprint(pages_blueprint, url_prefix="")
 app.register_blueprint(licco_ws_blueprint, url_prefix="/ws")
-
-
-def create_notifier(app: Flask) -> Notifier:
-    notifications_config = app.config["NOTIFICATIONS"]
-    if not notifications_config:
-        return Notifier("", EmailSenderMock(), "")
-
-    email_sender = EmailSenderMock()
-    email_config = notifications_config["email"]
-    if email_config and not email_config["development_mode"]:
-        email_sender = EmailSender(EmailSettings(email_config["url"], email_config["port"],
-                                                 email_config["email_auth"],
-                                                 email_config["user"], email_config["password"],
-                                                 email_config["username_to_email_service"]))
-    service_url = notifications_config["service_url"]
-    admin_email = notifications_config["email"]["admin_email"]
-
-    return Notifier(service_url, email_sender, admin_email)
-
-
-context.notifier = create_notifier(app)
-
-initialize_collections(context.licco_db)
 
 
 if __name__ == '__main__':
